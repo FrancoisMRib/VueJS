4ea035dc3002c6d051fc310cc8e914e6888097d1
FracoisMRib <fr.ribere@gmail.com>
2023-12-13 11:52:04 +0100

    Exo UnAmi VueJS - replacement composants par tableau

---
+++ App.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- <div class="container"> -->
+    <h1 class="text-center">Les Amis</h1>
+    <ul class="list-group">
+      <un-ami v-for="(unAmi) in lesAmis" :key="unAmi.id" :leNom="unAmi.name" :lePhone="unAmi.phone" :leMail="unAmi.email" :premium=true></un-ami>
+      <!--<un-ami leNom="Steven Seagal" lePhone="1234567" leMail="steven@seagal.com" premium="1"></un-ami>
+      <un-ami leNom="Gérard Menvuça" lePhone="0987654" leMail="gérard@menvuça.com" premium="0"></un-ami>   
+      <un-ami leNom="Jacques Arton" lePhone="023456578" leMail="carton@lolilol.com" premium="1"></un-ami>-->
+    </ul>
+  <!-- </div> -->
+</template>
+
+<script>
+export default{
+  data(){
+    return {
+      lesAmis: [
+        {
+            id: 'lasticot',
+            name: 'COCO L ASTICOT',
+            phone: '01234 5678 991',
+            email: 'coco@lasticot.com',
+        },
+        {
+            id: 'janine',
+            name: 'Janine DeLavega',
+            phone: '09876 543 221',
+            email: 'janine@delavega.com',
+        },
+    ],
+    }
+  },
+}
+</script>
---
+++ components/UnAmi.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="container my-1">
+        <ul class="list-group">
+            <h2 class="list-group-item">{{leNom}}{{premiumData == true ? '(Ami Premium)' : ' (Ami Nul)'}}</h2> 
+            <!--<h3>{{premiumData == '1' ? ' - Ami Premium' : ' - Ami Nul'}}</h3>-->
+            <button @click="afficherPremium" class="btn btn-danger">Premium ?</button>
+            <button @click="afficherDetails" class="btn btn-primary">Voir Détails</button>           
+            <ul v-if="detailsVisibles" class="list-group">
+                <li class="list-group-item">{{lePhone}}</li>
+                <li class="list-group-item">{{leMail}}</li>
+            </ul>
+        </ul>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    //props: ["leNom", "lePhone", "leMail", "premium"],
+    props : {
+        leNom : {
+            type:String,
+            required: true
+        },
+        lePhone : {
+            type:String,
+            required: true
+        },
+        leMail : {
+            type:String,
+            required: true
+        },
+        premium : {
+            type:Boolean,
+            default: false,
+            //validator: function (value) {
+            //    return value === '1' || value === '0';
+            //}
+            required: false
+        }
+    },
+    data(){
+        return{
+            
+            premiumData: this.premium,
+            detailsVisibles:false,
+/*
+            prop1:{
+                leNom: 'Steven Seagal',
+                lePhone: '1234567',
+                leMail: 'steven@seagal.com',
+            },
+            prop2:{
+                leNom: 'Gérard Mavuça',
+                lePhone: '0987654',
+                leMail: 'gérard@mavuça.com',
+            },
+
+            unPote:{
+                id:'jojo',
+                name:"Steven Seagal",
+                phone:'123 12346 24',
+                email:'steven@seegar.com',
+            },
+
+            unPote2: {
+                id:'jojo',
+                name:"Gérard Menvuça",
+                phone:'123 45678 99',
+                email:'gege@menvu.com',
+            },
+            */
+        }
+    },
+    methods:{
+        afficherDetails(){
+            this.detailsVisibles = !this.detailsVisibles;
+        } ,
+        afficherPremium() {
+            this.premiumData = !this.premiumData ;
+            /*if (this.premiumData === '1') {
+                this.premiumData = '0' ;
+            } else {
+                this.premiumData = '1' ;
+            }*/
+        }
+    },
+}
+</script>
